ae87928ef378dfc33e775215317992c36aa90a26
Claudio-Boza <cclaudiowillian@gmail.com>
2024-03-01 17:33:09 -0400

    add widget titletext

--- lib/pages/page1.dart
+++ lib/pages/page1.dart
@@ -1,3 +1,4 @@
+import 'package:estudo_dio_flutter/widgets/title_age.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -14,64 +15,97 @@ class _Page1State extends State<Page1> {
 
   List<String> sexo = ['Masculino', 'Femino'];
   String? valueRadio;
+  bool vagasPython = false;
+  bool vagasJava = false;
+  bool vagasPascal = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: textController,
-              decoration: const InputDecoration(
-                labelText: 'Nome: ',
-                border: OutlineInputBorder(),
+      body: ListView(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const TitleText(title: 'Nome:'),
+              TextField(
+                controller: textController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-            const Text('Idade:'),
-            TextField(
-              controller: dateController,
-              readOnly: true,
-              onTap: () async {
-                await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(1500, 4, 22),
-                  lastDate: DateTime(2050, 12, 31),
-                ).then((value) {
-                  if (value == null) {
-                    return;
-                  } else {
-                    setState(() {
-                      dateController.text =
-                          DateFormat('dd/MM/yyyy').format(value);
-                    });
-                  }
-                });
-              },
-            ),
-            const Text('Sexo:'),
-            Column(
-              children: sexo.map((e) {
-                return RadioListTile(
-                  title: Text(e),
-                  value: e,
-                  selected: e == valueRadio ? true : false,
-                  groupValue: valueRadio,
-                  onChanged: (value) {
-                    setState(() {
-                      valueRadio = value;
-                    });
-                  },
-                );
-              }).toList(),
-            )
-          ],
-        ),
-      ),
+              const TitleText(title: 'Idade:'),
+              TextField(
+                controller: dateController,
+                readOnly: true,
+                onTap: () async {
+                  await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(1500, 4, 22),
+                    lastDate: DateTime(2050, 12, 31),
+                  ).then((value) {
+                    if (value == null) {
+                      return;
+                    } else {
+                      setState(() {
+                        dateController.text =
+                            DateFormat('dd/MM/yyyy').format(value);
+                      });
+                    }
+                  });
+                },
+              ),
+              const TitleText(title: 'Sexo:'),
+              Column(
+                children: sexo.map((e) {
+                  return RadioListTile(
+                    title: Text(e),
+                    value: e,
+                    selected: e == valueRadio ? true : false,
+                    groupValue: valueRadio,
+                    onChanged: (value) {
+                      setState(() {
+                        valueRadio = value;
+                      });
+                    },
+                  );
+                }).toList(),
+              ),
+              const TitleText(title: 'Vagas:'),
+              CheckboxListTile(
+                title: const Text('DEV Python'),
+                value: vagasPython,
+                onChanged: (value) {
+                  setState(() {
+                    vagasPython = !vagasPython;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                title: const Text('DEV Java'),
+                value: vagasJava,
+                onChanged: (value) {
+                  setState(() {
+                    vagasJava = !vagasJava;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                title: const Text('DEV Object Pascal'),
+                value: vagasPascal,
+                onChanged: (value) {
+                  setState(() {
+                    vagasPascal = !vagasPascal;
+                  });
+                },
+              )
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
